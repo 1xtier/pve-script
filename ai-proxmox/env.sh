019aabd5b3fd0ead2f02982f3ca6bb52923c12b0
1x:tier <soraxsociety@gmail.com>
2025-08-06 17:53:57 -0400

    work with ai-proxmox

--- ai-proxmox/env.sh
+++ ai-proxmox/env.sh
@@ -5,6 +5,10 @@ function add-repo() {
   sudo chmod +r /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
   sudo echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" >> /etc/apt/sources.list
 }
+function install_pkg() {
+  apt update 
+  apt install proxmox-auto-install-assistant
+}
 function download() {
   wget https://enterprise.proxmox.com/iso/proxmox-ve_8.3-1.iso
 }
