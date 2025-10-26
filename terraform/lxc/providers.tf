86439e60928159cac400574d35019a6feeba3fb7
1xtier <soraxsociety@gmail.com>
2025-10-26 16:06:00 +0400

    Delete terraform lxc

--- terraform/lxc/providers.tf
+++
@@ -1,7 +0,0 @@
-provider "proxmox" {
-  pm_tls_insecure     = true
-  pm_api_url          = ""
-  pm_api_token_secret = ""
-  pm_api_token_id     = ""
-  pm_otp = ""
-}
--- terraform/lxc/pve.auto.tfvars
+++
@@ -1,3 +0,0 @@
-pve_api_url = ""
-pve_api_token_id = ""
-pve_api_token_secret = ""
